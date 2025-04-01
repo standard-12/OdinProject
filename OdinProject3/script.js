dc4393068a97488150dad4a53081051a6bca7120
shreenath <shreenathshenoy12@gmail.com>
2025-04-01 09:47:25 +0530

    Working on javascript

--- OdinProject3/script.js
+++ OdinProject3/script.js
@@ -14,12 +14,57 @@ function getRandInteger(min,max) {
     return Math.floor(Math.random()*(max-min))+min;
 }
 
-function getHumanChoice() {
-    return prompt("Enter Your Choice").toLowerCase()
+// function getHumanChoice() {
+//     return prompt("Enter Your Choice").toLowerCase()
     
-}
+// }
 
-function PlayRound(HumanChoice,ComputerChoice){
+// function PlayRound(HumanChoice,ComputerChoice){
+//     if (HumanChoice===ComputerChoice){
+//         console.log("Ohh it was a draw")
+//         return
+//     }
+//     else if(HumanChoice==="rock" && ComputerChoice === "scissor"){
+//         HumanScore++;
+//         console.log("You Won Rock beats scissor")
+//     }
+//     else if(HumanChoice==="paper" && ComputerChoice === "rock"){
+//         HumanScore++;
+//         console.log("You Won Paper beats Rock")
+//     }
+//     else if(HumanChoice==="scissor" && ComputerChoice==="paper"){
+//         HumanScore++;
+//         console.log("You Won Scissor beats Paper")
+//     }
+//     else{ 
+//         ComputerScore++;
+//         console.log("You Lose",ComputerChoice," beats ",HumanChoice)
+//     }
+//     return
+// }
+
+
+// let HumanScore=0;
+// let ComputerScore=0;
+
+// while(HumanScore<3 && ComputerScore<3){
+//     let ComputerChoice=getComputerChoice();
+//     console.log("Comp Choice is :",ComputerChoice)
+//     let HumanChoice=getHumanChoice();
+//     console.log("Human Choice is :",HumanChoice)
+//     PlayRound(HumanChoice,ComputerChoice)
+// }
+
+// if(HumanScore === 3){
+//     console.log("Human Won")
+// }
+// else console.log("Computer Won")
+let HumanScore=0;
+let ComputerScore=0;
+function PlayRound(HumanChoice){
+    let ComputerChoice=getComputerChoice();
+    console.log("Comp Choice is :",ComputerChoice)
+
     if (HumanChoice===ComputerChoice){
         console.log("Ohh it was a draw")
         return
@@ -40,22 +85,33 @@ function PlayRound(HumanChoice,ComputerChoice){
         ComputerScore++;
         console.log("You Lose",ComputerChoice," beats ",HumanChoice)
     }
-    return
+
+    if(HumanScore === 3){
+        alert("Game Over")
+        console.log("Human Won")
+    }
+    else if (ComputerScore==3) {
+        console.log("Computer Won")
+        alert("Game Over");
+    }
+    else
+    return;
 }
 
+const rockbtn=document.querySelector(".rock")
+rockbtn.onclick = () => PlayRound("rock");
 
-let HumanScore=0;
-let ComputerScore=0;
+const scissorbtn=document.querySelector(".scissor")
+scissorbtn.onclick = () => PlayRound("scissor");
 
-while(HumanScore<3 && ComputerScore<3){
-    let ComputerChoice=getComputerChoice();
-    console.log("Comp Choice is :",ComputerChoice)
-    let HumanChoice=getHumanChoice();
-    console.log("Human Choice is :",HumanChoice)
-    PlayRound(HumanChoice,ComputerChoice)
-}
+const paperbtn = document.querySelector(".paper")
+paperbtn.onclick = () => PlayRound("paper");
 
-if(HumanScore === 3){
-    console.log("Human Won")
-}
-else console.log("Computer Won")
+
+
+
+
+
+
+
+
